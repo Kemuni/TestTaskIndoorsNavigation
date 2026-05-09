26f22fd9ca4e28881481b6cec6e20b37b607f8cb
JFurB <dimabondarenko471@gmail.com>
2026-05-10 02:43:31 +0300

    fix(backend): Перенести логгер в начало функции перехвата и настроить порядок обработки ошибок.

--- backend_service/core/exception_handler.py
+++ backend_service/core/exception_handler.py
@@ -1,7 +1,7 @@
 import logging
 
 from django.db import IntegrityError
-from rest_framework import status
+from rest_framework import status, exceptions as exceptions_drf
 from rest_framework.exceptions import ValidationError, APIException
 from rest_framework.request import Request
 from rest_framework.response import Response
@@ -12,6 +12,11 @@ from core.serializers import BaseResponseSerializer
 logger = logging.getLogger(__name__)
 
 def exception_handler(exc, context):
+    """ Обрабатываем каждую ошибку API и возвращаем обработанный Response """
+    request: Request = context['request']
+    logger.error(f"{request.method} request to {request.get_full_path()} failed: {exc}", exc_info=True)
+    print(type(exc))
+
     error_details = {
         'code': 'UNKNOWN',
         'message': 'Unexpected error. Please try again later.'
@@ -35,23 +40,25 @@ def exception_handler(exc, context):
         error_details["message"] = error_details["message"].strip()
         http_status_code = status.HTTP_422_UNPROCESSABLE_ENTITY
 
-    elif isinstance(exc, APIException):
-        error_details["code"] = error_details["message"] = str(exc.__class__.__name__)
-        http_status_code = getattr(exc, 'status_code', status.HTTP_400_BAD_REQUEST)
-
     elif isinstance(exc, IntegrityError):
         # Ошибки базы данных
         error_details["code"] = "DATABASE_INTEGRITY_ERROR"
         error_details["message"] = str(exc)
         http_status_code = status.HTTP_409_CONFLICT
 
+    elif isinstance(exc, exceptions_drf.PermissionDenied):
+        error_details["code"] = "PERMISSION_DENIED"
+        error_details["message"] = str(exc)
+        http_status_code = status.HTTP_403_FORBIDDEN
+
     elif isinstance(exc, PermissionError):
         error_details["code"] = "PERMISSION_DENIED"
         error_details["message"] = "You do not have permission for this."
         http_status_code = status.HTTP_403_FORBIDDEN
 
-    request: Request = context['request']
-    logger.error(f"{request.method} request to {request.get_full_path()} failed: {exc}", exc_info=True)
+    elif isinstance(exc, exceptions_drf.APIException):
+        error_details["code"] = error_details["message"] = str(exc.__class__.__name__)
+        http_status_code = getattr(exc, 'status_code', status.HTTP_400_BAD_REQUEST)
 
     return Response(
         BaseResponseSerializer({
