cec44c0bed9ca5316ab89d4f6fa2fb812c8bb190
JFurB <dimabondarenko471@gmail.com>
2026-05-10 04:18:09 +0300

    feat(backend): Добавить Django контейнер в docker-compose с поддержкой watch.

--- backend_service/backend_service/settings.py
+++ backend_service/backend_service/settings.py
@@ -27,6 +27,7 @@ DEBUG = os.getenv('DEBUG', True)
 ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
+    '0.0.0.0',
 ]
 
 
@@ -85,8 +86,12 @@ WSGI_APPLICATION = 'backend_service.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.getenv('POSTGRES_DB', 'CatsDb'),
+        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD', 'postgres'),
+        'HOST': os.getenv('POSTGRES_SERVER', 'localhost'),
+        'PORT': os.getenv('POSTGRES_PORT', '5432'),
     }
 }
 
